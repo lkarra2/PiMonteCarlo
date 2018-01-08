b6eb4051078fe03602c0ae09d651263c9050d389
Manaswi Karra <lkarra2@uic.edu>
2018-01-08 17:31:33 -0600

    Using map function and runif

--- build.sbt
+++ build.sbt
@@ -2,5 +2,13 @@ name := "PiMonteCarlo"
 
 version := "0.1"
 
-scalaVersion := "2.12.4"
+scalaVersion := "2.12.0"
+
+scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature")
+
+libraryDependencies  ++= Seq(
+  "org.scalanlp" %% "breeze" % "0.13",
+  "org.scalanlp" %% "breeze-natives" % "0.13",
+  "org.ddahl" %% "rscala" % "2.5.0"
+)
         
--- src/main/scala/montecarlo/Montecarlo.scala
+++ src/main/scala/montecarlo/Montecarlo.scala
@@ -2,27 +2,44 @@ package montecarlo
 
 /**
   * Calculating pi using Monte Carlo Simulation
-  * Assuming Center at (0,0) and point in 1st Quadrant as (1,1)
   */
 
-import scala.util.Random
+import org.ddahl.rscala.RClient
+import scala.collection.mutable.ArrayBuffer
+import breeze.linalg._
+
 
 class Montecarlo {
   def calc(): Unit = {
-    val n = 100000000
-    var m = 0
+    val pInSq = 10000
 
-    val rand = new Random()
-    for (i <- 0 to n if({
-      val x = rand.nextFloat()
-      val y = rand.nextFloat()
-      ((x*x) + (y*y)) <= 1
-    })) m += 1
+    val R = RClient()
+    val points = new ArrayBuffer[Vector[Double]]
 
-    val rho = m.toFloat/n.toFloat
+    for( a <- 1 to pInSq) {
+      points += Vector[Double](R.evalD1("m <- runif(2)"))
+    }
+
+    val pInCircle = points.map(x=>pointWithinCircle(x)).filter(x => x == true).length
+    println("Points within Circle = " + pInCircle)
+
+    val rho = pInCircle.toFloat/pInSq.toFloat
     val pi = rho * 4
 
+    println("Rho = "+rho)
     println("Pi = " +pi)
+  }
+
+  //Taken from GK
+  def pointWithinCircle(p:Vector[Double]): Boolean = {
+    val x0 = 0.5
+    val y0 = 0.5
+    val radius = 0.5
+    val dist = scala.math.pow((x0 - p{0}), 2.0) + scala.math.pow((y0 - p{1}), 2)
+
+    if (dist < scala.math.pow(radius, 2))
+      return true
+    return false
   }
 }
 
