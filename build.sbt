cf9ae90c800078f7f668e64f01e5802902e774f6
Manaswi Karra <lkarra2@uic.edu>
2018-01-09 00:11:31 -0600

    Actor Framework to Calculate Pi

--- build.sbt
+++ build.sbt
@@ -6,9 +6,13 @@ scalaVersion := "2.12.0"
 
 scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature")
 
+lazy val akkaVersion = "2.5.3"
+
 libraryDependencies  ++= Seq(
   "org.scalanlp" %% "breeze" % "0.13",
   "org.scalanlp" %% "breeze-natives" % "0.13",
-  "org.ddahl" %% "rscala" % "2.5.0"
+  "org.ddahl" %% "rscala" % "2.5.0",
+  "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+  "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
+  "org.scalatest" %% "scalatest" % "3.0.1" % "test"
 )
-        
--- src/main/scala/montecarlo/Montecarlo.scala
+++ src/main/scala/montecarlo/Montecarlo.scala
@@ -1,34 +1,51 @@
-package montecarlo
+//#full-example
+package com.lightbend.akka.sample
 
 /**
-  * Calculating pi using Monte Carlo Simulation
+  * Author: LMK
+  * Monte Carlo Simulation for Calculating Pi, using the Actor Framework
   */
 
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
 import org.ddahl.rscala.RClient
 import scala.collection.mutable.ArrayBuffer
 import breeze.linalg._
 
+//#pimontecarlosimulator-companion
+object PiMonteCarlo {
+  //#pimontecarlosimulator-messages
+  def calc(message: String, printerActor: ActorRef): Props = Props(new PiMonteCarlo(message, printerActor))
+  //#greeter-messages
+  final case class piValue()
+  case object CalcPi
+}
 
-class Montecarlo {
-  //method to calculate and print value of pi
-  def calc(): Unit = {
+//#pimontecarlosimulator-actor
+class PiMonteCarlo(message: String, printerActor: ActorRef) extends Actor {
+  import PiMonteCarlo._
+  import Printer._
+
+  var currentPi: Float = 0
+
+  def receive = {
+    case piValue() =>
+      currentPi = calc()
+    case CalcPi           =>
+      //#pimontecarlosimulator-send-message
+      printerActor ! PrintingPi(currentPi)
+  }
+
+  def calc(): Float = {
     val pInSq = 10000
-
     val R = RClient()
     val points = new ArrayBuffer[Vector[Double]]
-
-    for( a <- 1 to pInSq) {
+    for (a <- 1 to pInSq) {
       points += Vector[Double](R.evalD1("m <- runif(2)"))
     }
-
-    val pInCircle = points.map(x=>pointWithinCircle(x)).filter(x => x == true).length
-    println("Points within Circle = " + pInCircle)
-
-    val rho = pInCircle.toFloat/pInSq.toFloat
-    val pi = rho * 4
-
-    println("Rho = "+rho)
-    println("Pi = " +pi)
+    val pInCircle = points.map(x => pointWithinCircle(x)).filter(x => x == true).length
+    val rho = pInCircle.toFloat / pInSq.toFloat
+    val piValue = rho * 4
+    return piValue
   }
 
   //method to check if point is within circle
@@ -44,9 +61,55 @@ class Montecarlo {
   }
 }
 
-object Run extends Montecarlo {
-  //main method
-  def main(args:Array[String]): Unit = {
-    calc()
+//#printer-companion
+object Printer {
+  //#printer-messages
+  def props: Props = Props[Printer]
+  //#printer-messages
+  final case class PrintingPi(pi: Float)
+}
+
+//#printer-actor
+class Printer extends Actor with ActorLogging {
+  import Printer._
+
+  def receive = {
+    case PrintingPi(pi) =>
+      log.info(s"Pi Value recieved (from ${sender()}): $pi")
   }
+}
+
+//#main-class
+object Montecarlo extends App {
+  import PiMonteCarlo._
+
+  // Create the 'helloAkka' actor system
+  val system: ActorSystem = ActorSystem("helloAkka")
+
+  //#create-actors
+  // Create the printer actor
+  val printer: ActorRef = system.actorOf(Printer.props, "printerActor")
+
+  // Create the 'pimontecarlosimulator' actors
+  val Actor1: ActorRef =
+    system.actorOf(PiMonteCarlo.calc("Actor1", printer), "Actor1")
+  val Actor2: ActorRef =
+    system.actorOf(PiMonteCarlo.calc("Actor2", printer), "Actor2")
+  val Actor3: ActorRef =
+    system.actorOf(PiMonteCarlo.calc("Actor3", printer), "Actor3")
+  //#create-actors
+
+  //#main-send-messages
+  Actor1 ! piValue()
+  Actor1 ! CalcPi
+
+  Actor1 ! piValue()
+  Actor1 ! CalcPi
+
+  Actor2 ! piValue()
+  Actor2 ! CalcPi
+
+  Actor3 ! piValue()
+  Actor3 ! CalcPi
+
 }
